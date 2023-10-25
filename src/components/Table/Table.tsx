a060e73967bd3f44bda223a50d25c2159fe91550
isqua <isqua@isqua.ru>
2023-10-25 14:22:31 +0300

    fix: remove filters

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -3,8 +3,6 @@ import React, { HTMLProps } from 'react'
 import './Table.css'
 
 import {
-    Column,
-    Table,
     ExpandedState,
     useReactTable,
     getCoreRowModel,
@@ -145,11 +143,6 @@ export function Table() {
                                                     header.column.columnDef.header,
                                                     header.getContext()
                                                 )}
-                                                {header.column.getCanFilter() ? (
-                                                    <div>
-                                                        <Filter column={header.column} table={table} />
-                                                    </div>
-                                                ) : null}
                                             </div>
                                         )}
                                     </th>
@@ -248,57 +241,6 @@ export function Table() {
             </div>
             <pre>{JSON.stringify(expanded, null, 2)}</pre>
         </div>
-    )
-}
-
-function Filter({
-    column,
-    table,
-}: {
-    column: Column<any, any>
-    table: Table<any>
-}) {
-    const firstValue = table
-        .getPreFilteredRowModel()
-        .flatRows[0]?.getValue(column.id)
-
-    const columnFilterValue = column.getFilterValue()
-
-    return typeof firstValue === 'number' ? (
-        <div className="flex space-x-2">
-            <input
-                type="number"
-                value={(columnFilterValue as [number, number])?.[0] ?? ''}
-                onChange={e =>
-                    column.setFilterValue((old: [number, number]) => [
-                        e.target.value,
-                        old?.[1],
-                    ])
-                }
-                placeholder={`Min`}
-                className="w-24 border shadow rounded"
-            />
-            <input
-                type="number"
-                value={(columnFilterValue as [number, number])?.[1] ?? ''}
-                onChange={e =>
-                    column.setFilterValue((old: [number, number]) => [
-                        old?.[0],
-                        e.target.value,
-                    ])
-                }
-                placeholder={`Max`}
-                className="w-24 border shadow rounded"
-            />
-        </div>
-    ) : (
-        <input
-            type="text"
-            value={(columnFilterValue ?? '') as string}
-            onChange={e => column.setFilterValue(e.target.value)}
-            placeholder={`Search...`}
-            className="w-36 border shadow rounded"
-        />
     )
 }
 
