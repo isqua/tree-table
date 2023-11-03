3871d55afaba272326310e82a4974b704d93490a
isqua <isqua@isqua.ru>
2023-11-03 14:31:01 +0300

    ci: set up linters and deployment

--- src/components/Table/IndeterminateCheckbox.tsx
+++ src/components/Table/IndeterminateCheckbox.tsx
@@ -4,12 +4,13 @@ export function IndeterminateCheckbox({
     indeterminate, className = '', ...rest
 }: { indeterminate?: boolean; } & HTMLProps<HTMLInputElement>) {
     const ref = React.useRef<HTMLInputElement>(null!);
+    const { checked } = rest;
 
     React.useEffect(() => {
         if (typeof indeterminate === 'boolean') {
-            ref.current.indeterminate = !rest.checked && indeterminate;
+            ref.current.indeterminate = !checked && indeterminate;
         }
-    }, [ref, indeterminate]);
+    }, [checked, indeterminate, ref]);
 
     return (
         <input
